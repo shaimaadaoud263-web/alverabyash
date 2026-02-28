1fc0f0f8a0c362d4e7ed5dab44cf5cbc7b3a3145
shaimaadaoud263-web <shaimaadaoud263@icloud.com>
2026-02-28 02:22:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -112,13 +112,13 @@ if (new Date() <= discountEndDate && subtotal > 0) {
 
     discountBox.innerHTML = `
         <div style="color:#d63384; font-weight:bold;">
-            🎉 Grand Opening Offer (${discountpercent}% OFF)
+            🎉 Grand Opening Offer (${discountPercent}% OFF)
         </div>
         <div style="text-decoration: line-through; color: gray;">
             ${subtotal.toFixed(2)} EGP
         </div>
         <div style="color: green; font-size:18px; font-weight:bold;">
-            You Save ${25.toFixed(2)} EGP
+            You Save ${discountAmount.toFixed(2)} EGP
         </div>
     `;
 } else {
@@ -212,6 +212,7 @@ document.querySelectorAll('.price').forEach(priceEl => {
 });
     });
 }
+
 
 
 
